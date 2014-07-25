e3fd069e4e448b183db24e9862151ec3bcaae76a
Liam Westby <liamwestby@icloud.com>
2014-07-25 10:40:21 +0100

    Top donors now auto refresh

--- scripts/topdonors.js
+++ scripts/topdonors.js
@@ -11,3 +11,25 @@ function dataCallback(data) {
 		$(amountElement).text(amount);
 	}
 }
+
+function ajaxVersion() {
+	alert("calling");
+	$.ajax({
+    	url: 'http://imraising.com/minethon/json/livedata.jsonp',
+        dataType: 'jsonp',
+        success: function(dataWeGotViaJsonp){
+        	var top = data.topDonors;
+			for (var i = 1; i <= 6; i++) {
+				var nameElement = "#d" + i + "h";
+				var amountElement = "#d" + i + "p";
+				
+				var name = i + ". " + top[i-1].screen;
+				var amount = "$" + top[i-1].total;
+				
+				$(nameElement).text(name);
+				$(amountElement).text(amount);
+			}
+        }
+    });
+    setTimeout(ajaxVersion, 30000);
+}
